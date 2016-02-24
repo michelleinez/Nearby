9e4282a559f43de312a2a465f86086c789b735fe
Barclay Iversen <barclayiversen@gmail.com>
2016-02-23 18:32:01 -0800

    got Eli's styling integrated with my FB functions

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,18 +5,16 @@ class Main extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		//$this->output->enable_profiler();
-	}
-
-	public function index()
-	{
-
-		$this->load->view('index');
-	}
-
-	public function facebook_login()
-	{
-
 		$this->load->model('login');
+	}
+
+	public function index(){
+		$this->load->view('main');
+	}
+
+	public function facebook_login(){
+
+
 		$user = array(
 				'first_name' => $this->input->post('first_name'),
 				'last_name' => $this->input->post('last_name'),

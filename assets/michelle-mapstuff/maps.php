4d92ae324ff5a5198af2e3dbcb9ab6904a8a4398
Michelle <chinbule@gmail.com>
2016-02-24 14:28:04 -0800

    Geolocation of maps.

--- assets/michelle-mapstuff/maps.php
+++ assets/michelle-mapstuff/maps.php
@@ -7,14 +7,18 @@
     <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
   </head>
   <body>
+    <button class="results1"></button>
     <form id="searchform1">
-      <input type="text" id="searchbox1" />
-      <input type="text" id="searchbox2" />
-      <input type="submit" />
+      Find
+      <input type="text" id="searchbox1" style="display: inline-block"/>
+      with
+      <input type="text" id="searchbox2" style="display: inline-block"/>
+      nearby...
+      <input type="submit" value="Go!"/>
     </form>
 
     <div id="map"></div>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLXNzw4zstgAtHYCIUXHH9pl4HNQGgIUs&signed_in=true&libraries=places&callback=initMap" async defer></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2KaKcKloJYNmCAUJ7K342whmwBfgj_I0&signed_in=true&libraries=places&callback=initMap" async defer></script>
     <script src="/js/map.js"></script>
   </body>
 </html>

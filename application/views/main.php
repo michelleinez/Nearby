5fc903c10fe0f5a599170795b2114510f39d5c43
Michelle <chinbule@gmail.com>
2016-02-25 10:44:41 -0800

    Variables can now be passed between callbacks.

--- application/views/main.php
+++ application/views/main.php
@@ -3,36 +3,39 @@
 <head>
 	<meta charset="utf-8">
 	<title>Nearby</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/map.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 </head>
 <body>
 	<div class="row">
-		<form class="col s12">
+		<form class="col s12" id="searchform1">
 			<div class="row">
 				<div class='col l1 offset-l1'>
 					<label class="search-label">Find a</label>
 				</div>
 				<div class="input-field col l3">
-					<input placeholder="movie theater" name="place_1" type="text" class="validate search-text">
+					<input id="searchbox1" placeholder="movie theater" name="place_1" type="text" class="validate search-text">
 				</div>
 				<div class='col l1'>
 					<label class="search-label">with</label>
 				</div>
 				<div class="input-field col l3">
-					<input placeholder="ice cream" name="place_2" type="text" class="validate search-text">
+					<input id="searchbox2" placeholder="ice cream" name="place_2" type="text" class="validate search-text">
 				</div>
 				<div class='col l1'>
 					<label class="search-label">nearby!</label>
 				</div>
 				<div class='col l1'>
-					<a class="btn-floating btn-medium waves-effect waves-light blue search-button"><i class="material-icons">search</i></a>
+					<button type = 'submit' id="search-button" class="btn-floating btn-medium waves-effect waves-light blue search-button"><i class="material-icons">search</i></button>
 				</div>
 			</div>
 		</form>
 	</div>
 
 	<!-- temp image -  Map will go here -->
-	<img src="assets/temp-map.png" alt="temp-map" style="display:block;margin:auto;"/>
-
+	<div id="map" style="height: 65%;"></div>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2KaKcKloJYNmCAUJ7K342whmwBfgj_I0&signed_in=true&libraries=places&callback=initMap" async defer></script>
+	<script src="/assets/js/map.js"></script>
 </body>
 
 </html>
